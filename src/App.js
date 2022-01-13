cdf6d8855d764c0dad3df4d9a3f2af49715917e1
AndriyAndrusyshyn <andrusyshyn_a@hotmail.com>
2022-01-13 17:05:05 +0200

    homework reakt first half

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import Notebook from "./components/notebook/Notebook";
 import Notebooks from "./components/Notebooks/Notebooks";
 
-import './Spacex/spacex';
+
 import Users from "./components/Users/Users";
 import Posts from "./components/Posts/posts";
 import Comments from "./components/Comments/comments";
 
 import './style.css'
-import Flights from "./Flights/flights";
+
 
 
 const App = () => {
 
     return (
         <div>
-    {/*    <div className={'main'}>*/}
-    {/*        <Users/>*/}
-    {/*        <Posts/>*/}
-    {/*    </div>*/}
-    {/*        <hr/>*/}
-    {/*    <div className={'coment'}>*/}
-    {/*<Comments/>*/}
-    {/*    </div>*/}
-        <Spacex/>
+        <div className={'main'}>
+            <Users/>
+            <Posts/>
+        </div>
+            <hr/>
+        <div className={'coment'}>
+    <Comments/>
+        </div>
         </div>
     );
 
